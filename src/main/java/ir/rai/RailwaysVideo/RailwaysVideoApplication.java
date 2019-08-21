a8c33a73ebd7393e4c3d79b38bcd819abe556c3b
ruperaz <ruperaz@gmail.com>
2019-08-21 11:04:20 +0430

    init

---
+++ src/main/java/ir/rai/RailwaysVideo/RailwaysVideoApplication.java
@@ -0,0 +1,13 @@
+package ir.rai.RailwaysVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaysVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaysVideoApplication.class, args);
+	}
+
+}
